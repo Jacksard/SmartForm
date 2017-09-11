f04b215f4ca5eb2bf9b7a0ac9c79830aa060d16b
jacobgoskha <sardaukarkk@gmail.com>
2017-09-11 11:27:49 -0400

    fixed search fold issue

---
+++ js/data.js
@@ -0,0 +1,51 @@
+console.log('Array in data.js has been loaded');
+var myArr = [
+    {
+        first_name: 'Robert',
+        last_name: 'Collen',
+        age: 33,
+        email: "Jane@gmail.com"
+    },
+    {
+        first_name: 'Jane',
+        last_name: 'Smith',
+        age: 22,
+        email: "JaneSmith@gmail.com"
+    },
+    {
+        first_name: 'Kate',
+        last_name: 'Jones',
+        age: 33,
+        email: "Jones.Kate@gmail.com"
+    },
+    {
+        first_name: 'Shelly',
+        last_name: 'Osborn',
+        age: 33,
+        email: "Shelly123@gmail.com"
+    },
+    {
+        first_name: 'Sam',
+        last_name: 'Colts',
+        age: 26,
+        email: "SamColts@gmail.com"
+    },
+    {
+        first_name: 'Sam',
+        last_name: 'Kas',
+        age: 26,
+        email: "Sam-kas@gmail.com"
+    },
+    {
+        first_name: 'Mike',
+        last_name: 'Sims',
+        age: 18,
+        email: "MikeSims@gmail.com"
+    },
+    {
+        first_name: 'Thomas',
+        last_name: 'Roberts',
+        age: 41,
+        email: "ThomasR@gmail.com"
+    }
+];
--- js/main.js
+++ js/main.js
@@ -6,62 +6,7 @@ $(document).ready(function(){
    
 });
 
-
-
-
-var myArr = [
-    {
-        first_name: 'Robert',
-        last_name: 'Collen',
-        age: 33,
-        email: "Jane@gmail.com"
-    },
-    {
-        first_name: 'Jane',
-        last_name: 'Smith',
-        age: 22,
-        email: "JaneSmith@gmail.com"
-    },
-    {
-        first_name: 'Kate',
-        last_name: 'Jones',
-        age: 33,
-        email: "Jones.Kate@gmail.com"
-    },
-    {
-        first_name: 'Shelly',
-        last_name: 'Osborn',
-        age: 33,
-        email: "Shelly123@gmail.com"
-    },
-    {
-        first_name: 'Sam',
-        last_name: 'Colts',
-        age: 26,
-        email: "SamColts@gmail.com"
-    },
-    {
-        first_name: 'Sam',
-        last_name: 'Kas',
-        age: 26,
-        email: "Sam-kas@gmail.com"
-    },
-    {
-        first_name: 'Mike',
-        last_name: 'Sims',
-        age: 18,
-        email: "MikeSims@gmail.com"
-    },
-    {
-        first_name: 'Thomas',
-        last_name: 'Roberts',
-        age: 41,
-        email: "ThomasR@gmail.com"
-    }
-];
-
 comesback = {};
-
 
 var myApp = angular.module("myApp", []);
 myApp.controller('add', function($scope){
@@ -73,11 +18,7 @@ myApp.controller('add', function($scope){
     comesback.age = person.age;
     comesback.email = person.email;
     console.log(comesback);
-
-
 };
-
-
 
     $scope.myArrayFunction = function (){
     console.log('I was pushed');
@@ -95,8 +36,6 @@ myApp.controller('add', function($scope){
         myArr.push(newPerson);
         }
 };
-
-
 
 $scope.myPerson = comesback;
 $scope.collection = myArr;
